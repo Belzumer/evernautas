27fb3b4668089986de6ddaebc7b0d173e3a4ec6c
jpcuencas <juan.pedro.cuenca.sevilla@gmail.com>
2018-10-20 21:45:09 +0200

    Core 2.1

--- app/src/androidTest/java/com/evernatuas/undisaster/dao/PlanDaoTest.java
+++ app/src/androidTest/java/com/evernatuas/undisaster/dao/PlanDaoTest.java
@@ -6,6 +6,7 @@ import android.support.test.runner.AndroidJUnit4;
 import android.util.Log;
 
 import com.evernatuas.undisaster.dao.impl.PlanAccionDao;
+import com.evernatuas.undisaster.dto.PlanAccion;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,6 +20,8 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class PlanDaoTest {
+    PlanAccion planAccion;
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -27,10 +30,46 @@ public class PlanDaoTest {
         assertEquals("com.evernatuas.undisaster", appContext.getPackageName());
     }
 
+    private void setObject() {
+        planAccion = new PlanAccion();
+        planAccion.setId(1L);
+        planAccion.setTitulo("Ejemplo");
+        planAccion.setPosicion(1);
+        planAccion.setDescripcion("Example");
+    }
+
     @Test
     public void getAll() {
         Context context = InstrumentationRegistry.getTargetContext();
         PlanAccionDao dao = new PlanAccionDao(context);
         Log.d(PlanAccionDao.class.getName(), dao.getAll(context).toString());
+    }
+
+    @Test
+    public void add() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        PlanAccionDao dao = new PlanAccionDao(context);
+        Log.d(PlanAccionDao.class.getName(), dao.add(context, planAccion).toString());
+    }
+
+    @Test
+    public void get() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        PlanAccionDao dao = new PlanAccionDao(context);
+        Log.d(PlanAccionDao.class.getName(), dao.get(context, 1L).toString());
+    }
+
+    @Test
+    public void update() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        PlanAccionDao dao = new PlanAccionDao(context);
+        dao.update(context, planAccion);
+    }
+
+    @Test
+    public void remove() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        PlanAccionDao dao = new PlanAccionDao(context);
+        dao.remove(context, planAccion);
     }
 }
